7af18d72b04e7c5706806969d7942bc238c0a3d7
Jaakko Suutarla <jaakko.suutarla@bf-engineering.fi>
2011-06-16 11:48:08 +0200

    Small fixes ;p

--- lib/gibbon_sts.rb
+++ lib/gibbon_sts.rb
@@ -74,11 +74,11 @@ module GibbonSTS
       def transform_to_sts_format(message)
         # Message will be Mail::Message
         sts_message = {}
-        sts_message[:html] = message.body
+        sts_message[:html] = message.body.to_s
         sts_message[:from_email] = message.from.is_a?(Array) ? message.from.first : message.from
         sts_message[:subject] = message.subject
         sts_message[:to_email] = message.to
-        sts_message[:reply_to] = message.reply_to.is_a?(Array) ? message.reply_to.first : message.reply_to
+        sts_message[:reply_to] = message.reply_to.is_a?(Array) ? message.reply_to.first : message.reply_to unless message.reply_to.nil?
         sts_message[:from_name] = message.from.first
         sts_message
       end
